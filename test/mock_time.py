bdd57c461456260cec315f4a5a584d1921cc6785
matt-hammond-bbc <matt.hammond@bbc.co.uk>
2015-02-04 18:26:16 +0000

    Improvements to 'time' module mock, including sleep() function, in preparation to test dvbcss.task

--- test/mock_time.py
+++ test/mock_time.py
@@ -16,23 +16,85 @@
 
 import dvbcss.monotonic_time as time
 
+import threading
+
 class MockTime(object):
+    """\
+    Object that provides mocks for time.time() and time.sleep()
+    
+    Use :func:`install` and :func:`uninstall` to plug the mock in.
+    """
 
     def __init__(self):
         super(MockTime,self).__init__()
-        self.timeNow = 0
+        self._timeNow = 0
+        self.doctoredTimeModule=None
         self.oldTimeFunc = None
-
+        self.oldSleepFunc = None
+        self.sleepEvents = []
+        
+    @property
+    def timeNow(self):
+        return self._timeNow
+        
+    @timeNow.setter
+    def timeNow(self, newValue):
+        self._timeNow = newValue
+        
+        # go through list of pending sleeps and wake any up that need to be,
+        # deleting them on the way.
+        i=0
+        while i<len(self.sleepEvents):
+            wakeUpTime, event = self.sleepEvents[i]
+            if self._timeNow >= wakeUpTime:
+                event.set()
+                del self.sleepEvents[i]
+            else:
+                i=i+1
+    
     def install(self, module=time):
-        if self.oldTimeFunc is None:
+        """\
+        Plugs the mock in (or does nothing if already plugged into something.
+        
+        :param module: The "time" module to have its `time()` and `sleep()` functions replaced. Defaults to the dvbcss.monotonic_time module.
+        """
+        if self.doctoredTimeModule is None:
+        
+            self.doctoredTimeModule = module
             self.oldTimeFunc = getattr(module,"time")
+            
             setattr(module,"time", self.mockTimeFunc)
+            self.oldSleepFunc = getattr(module,"sleep")
+            
+            setattr(module,"sleep", self.mockSleepFunc)
+        else:
+            raise Exception("MockTime is already plugged in. Cannot be plugged in in two places.")
+
 
     def mockTimeFunc(self):
         return self.timeNow
+        
+    def mockSleepFunc(self, durationSecs):
+        wakeUpTime = self.timeNow + durationSecs
+        event = threading.Event()
+        event.clear()
+        self.sleepEvents.append((wakeUpTime, event))
+        event.wait()
+        
 
-    def uninstall(self, module=time):
-        if self.oldTimeFunc is not None:
-            setattr(module,"time", self.oldTimeFunc)
+    def uninstall(self):
+        """\
+        Unplugs the mock (or does nothing if already unplugged.
+        """
+        if self.doctoredTimeModule is not None:
+        
+            setattr(self.doctoredTimeModule,"time", self.oldTimeFunc)
             self.oldTimeFunc=None
+            
+            setattr(self.doctoredTimeModule,"sleep", self.oldSleepFunc)
+            self.oldSleepFunc=None
+            
+            self.doctoredTimeModule = None
+        else:
+            raise Exception("MockTime is not already plugged in. Cannot be unplugged.")
             
